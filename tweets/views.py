c95592f537b619f6c8361b06e14a7493247d4f27
NamanMathur77 <namanmathur619@gmail.com>
2020-05-13 01:08:28 +0530

    Adding the retweet api serializer functionality

--- tweets/views.py
+++ tweets/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from .models import Tweet
 from .forms import TweetForm
-from .serializers import TweetSerializer, TweetActionSerializer
+from .serializers import TweetSerializer, TweetActionSerializer, TweetCreateSerializer
 from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
@@ -17,7 +17,7 @@ def home_view(request, *args, **kwargs):
 #@authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data=request.POST)
+    serializer = TweetCreateSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
         return Response(serializer.data, status=201)
@@ -59,6 +59,7 @@ def tweet_action_view(request, *args, **kwargs):
         data = serializer.validated_data
         tweet_id = data.get("id")
         action = data.get("action")
+        content = data.get("content")
         
         qs = Tweet.objects.filter(id=tweet_id)
         if not qs.exists():
@@ -72,7 +73,13 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == "unlike":
             obj.likes.remove(request.user)
         elif action == "retweet":
-            pass
+            new_tweet = Tweet.objects.create(
+                user=request.user, 
+                parent=obj, 
+                content=content
+                )
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
     return Response({}, status=200)
 
 
