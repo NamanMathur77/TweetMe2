afeecf300d70ba82d919c5fa5f0c4856c40af74f
NamanMathur77 <namanmathur619@gmail.com>
2020-05-13 22:14:59 +0530

    localizing the urls

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from .models import Tweet
+User = get_user_model()
 # Create your tests here.
+class TweetTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='abc', password='somepassword')
+        User.objects.create_user(username='cfe', password='somepassword')
+    def test_user_created(self):
+        user = User.objects.get(username="cfe")
+        self.assertEqual(user.username, "cfe")
