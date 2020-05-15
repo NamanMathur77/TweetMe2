c999067d4732f10554c9ea8c60c8d174e745cefb
NamanMathur77 <namanmathur619@gmail.com>
2020-05-16 00:58:24 +0530

    Setting up react for django project

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function loadTweets(callback) {
+  const xhr = new XMLHttpRequest()
+  const method = 'GET'
+  const url = "http://localhost:8000/api/tweets/"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function(){
+    callback(xhr.response, xhr.status)
+
+  }
+  xhr.send()
+}
+
 function App() {
+  const [tweets, setTweets] = useState([])
+
+  useEffect(()=>{
+    const myCallback = (response, status) => {
+      if (status === 200){
+        setTweets(response)
+      }
+    }
+    loadTweets(myCallback)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*xy3qx3iw^9oa7o-m*4z8_yc*@@$1ol&6r6@r#$+_^e@3$$8i+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 LOGIN_URL = "/login"
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS = ["like", "unlike", 'retweet']
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third-party
+    'corsheaders',
     'rest_framework',
     #internal
     'tweets',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 DEFAULT_RENDERER_CLASSES = [
     'rest_framework.renderers.JSONRenderer',
 ]
