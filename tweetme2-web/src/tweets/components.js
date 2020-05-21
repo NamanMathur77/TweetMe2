44bb666c2b8ea3b52ca7eea17f9a4eb104c308cc
NamanMathur77 <namanmathur619@gmail.com>
2020-05-21 12:46:11 +0530

    adding actionbtn api

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -1,139 +1,108 @@
-import React, { useEffect, useState } from "react";
-import { apiTweetList, apiTweetCreate } from "./lookup";
+import React, {useEffect, useState}  from 'react'
+
+import {
+  apiTweetAction,
+  apiTweetCreate, 
+  apiTweetList} from './lookup'
 
 export function TweetsComponent(props) {
-  const textAreaRef = React.createRef(); //through reference we can access the referenced field
-  const [newTweets, setNewTweets] = useState([])
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newVal = textAreaRef.current.value;
-    console.log(newVal)
-    let tempNewTweets = [...newTweets] //copying the newTweets state declared earlier
-    apiTweetCreate(newVal, (response, status)=>{
-      if (status===201){
+    const textAreaRef = React.createRef()
+    const [newTweets, setNewTweets] = useState([])
+    
+    const handleBackendUpdate = (response, status) =>{
+      // backend api response handler
+      let tempNewTweets = [...newTweets]
+      if (status === 201){
         tempNewTweets.unshift(response)
         setNewTweets(tempNewTweets)
-
-      } else{
+      } else {
         console.log(response)
-        alert ("An error occured")
+        alert("An error occured please try again")
       }
-    })
+    }
 
-    textAreaRef.current.value = "";
-  };
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const newVal = textAreaRef.current.value
+      // backend api request
+      apiTweetCreate(newVal, handleBackendUpdate)
+      textAreaRef.current.value = ''
+    }
+    return <div className={props.className}>
+            <div className='col-12 mb-3'>
+              <form onSubmit={handleSubmit}>
+                <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
 
-  return (
-    <div className={props.className}>
-      <div className="col-12 mb-3">
-        <form onSubmit={handleSubmit}>
-          <textarea
-            ref={textAreaRef}
-            className="form-control"
-            required={true}
-            name="tweet"
-          ></textarea>
-          <button type="submit" className="btn btn-primary my-3">
-            Tweet
-          </button>
-        </form>
-      </div>
-      <TweetsList newTweets={newTweets}/>
+                </textarea>
+                <button type='submit' className='btn btn-primary my-3'>Tweet</button>
+            </form>
+            </div>
+        <TweetsList newTweets={newTweets} />
     </div>
-  );
 }
 
 export function TweetsList(props) {
-  const [tweetsInit, setTweetsInit] = useState([]);
-  const [tweets, setTweets] = useState([])
-  const [tweetsDidSet, setTweetsDidSet] = useState(false)
-  // setTweetsInit([...props.newTweets].concat(tweetsInit))
-  useEffect(()=>{
-    const final = [...props.newTweets].concat(tweetsInit)
-    if (final.length !== tweets.length){
-      setTweets(final)
-    }
-  }, [props.newTweets, tweets, tweetsInit])
+    const [tweetsInit, setTweetsInit] = useState([])
+    const [tweets, setTweets] = useState([])
+    const [tweetsDidSet, setTweetsDidSet] = useState(false)
+    useEffect(()=>{
+      const final = [...props.newTweets].concat(tweetsInit)
+      if (final.length !== tweets.length) {
+        setTweets(final)
+      }
+    }, [props.newTweets, tweets, tweetsInit])
 
-  useEffect(() => {
-    if (tweetsDidSet === false){
-      const handleTweetListLookup = (response, status) => {
-        if (status === 200) {
-          setTweetsInit(response);
-          setTweetsDidSet(true)
-        } else {
-          alert("There was an error")
+    useEffect(() => {
+      if (tweetsDidSet === false){
+        const handleTweetListLookup = (response, status) => {
+          if (status === 200){
+            setTweetsInit(response)
+            setTweetsDidSet(true)
+          } else {
+            alert("There was an error")
+          }
         }
-      };
-      apiTweetList(handleTweetListLookup);
-    }
+        apiTweetList(handleTweetListLookup)
+      }
+    }, [tweetsInit, tweetsDidSet, setTweetsDidSet])
+    return tweets.map((item, index)=>{
+      return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
+    })
+  }
 
-  }, [tweetsInit,tweetsDidSet, setTweetsDidSet]);
-
-  return tweets.map((item, index) => {
-    return (
-      <Tweet
-        tweet={item}
-        className="my-5 py-5 border bg-white text-dark"
-        key={`${index}-{item.id}`}
-      />
-    );
-  });
-}
 
 export function ActionBtn(props) {
-  const { tweet, action } = props;
-  const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0);
-  const [userLike, setUserLike] = useState(
-    tweet.userLike === true ? true : false
-  );
-  const className = props.className ? props.className : "btn btn-primary";
-  const actionDisplay = action.display ? action.display : "Action";
-  const handleClick = (event) => {
-    event.preventDefault();
-    if (action.type === "like") {
-      if (userLike === true) {
-        //unklike it
-        setLikes(likes - 1);
-        setUserLike(false);
-      } else {
-        setLikes(likes + 1);
-        setUserLike(true);
+    const {tweet, action} = props
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    // const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
+    const className = props.className ? props.className : 'btn btn-primary btn-sm'
+    const actionDisplay = action.display ? action.display : 'Action'
+    
+    const handleActionBackendEvent = (response, status) =>{
+      console.log(response, status)
+      if (status === 200){
+        setLikes(response.likes)
+        // setUserLike(true)
       }
     }
-  };
-  const display =
-    action.type === "like" ? `${likes} ${actionDisplay}` : actionDisplay;
-
-  return (
-    <button className={className} onClick={handleClick}>
-      {" "}
-      {display}{" "}
-    </button>
-  );
-}
-
+    const handleClick = (event) => {
+      event.preventDefault()
+      apiTweetAction(tweet.id, action.type, handleActionBackendEvent)
+      
+    }
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+    return <button className={className} onClick={handleClick}>{display}</button>
+  }
+  
 export function Tweet(props) {
-  const { tweet } = props;
-  const className = props.className
-    ? props.className
-    : "col-10 mx-auto col-md-6";
-  return (
-    <div className={className}>
-      <p>
-        {tweet.id} - {tweet.content}
-      </p>
-      <div className="btn btn-group">
-        <ActionBtn tweet={tweet} action={{ type: "like", display: "like" }} />
-        <ActionBtn
-          tweet={tweet}
-          action={{ type: "unlike", display: "unlike" }}
-        />
-        <ActionBtn
-          tweet={tweet}
-          action={{ type: "retweet", display: "retweet" }}
-        />
-      </div>
+    const {tweet} = props
+    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    return <div className={className}>
+        <p>{tweet.id} - {tweet.content}</p>
+        <div className='btn btn-group'>
+          <ActionBtn tweet={tweet} action={{type: "like", display:"Likes"}}/>
+          <ActionBtn tweet={tweet} action={{type: "unlike", display:"Unlike"}}/>
+          <ActionBtn tweet={tweet} action={{type: "retweet", display:"Retweet"}}/>
+        </div>
     </div>
-  );
-}
+  }
--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -2,8 +2,12 @@ import {lookup} from '../lookup'
 
 export function apiTweetCreate(newTweet, callback){
     lookup("POST", "/tweets/create/", callback, {content: newTweet})
-  }
-  
-  export function apiTweetList(callback) {
-      lookup("GET", "/tweets/", callback)
-    }
+}
+export function apiTweetList(callback) {
+    lookup("GET", "/tweets/", callback)
+}
+
+export function apiTweetAction(tweetId, action, callback){
+    const data = {id: tweetId, action: action}
+    lookup("POST", "/tweets/action", callback, data)
+}
